deaf97d36bbff6fe111808a536ea38706c24f5a3
Ferdinand weijnschenk <F.L.Weynschenk@student.tudelft.nl>
2021-12-27 23:03:24 +0100

    initial backend connection

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,22 @@ import { createApp } from 'https://unpkg.com/petite-vue@0.2.2/dist/petite-vue.es
 import { loadString, loadUrl, setViewerSize } from './stlViewer.js'
 
 
+const vals = {};
+
 createApp({
     // getters
     get plusOne() {
         return this.count + 1
     },
     model: undefined,
-
-
     // methods
     renderStl() {
-        fetch('https://bp508.hopto.org:3847')
+        console.log(vals)
+        fetch('https://bp508.hopto.org:3847', {
+            method: 'POST',
+            body: JSON.stringify(vals),
+            headers: { 'Content-Type': 'application/json' }
+        })
             .then(response => response.json())
             .then(data => {
                 loadString(data);
@@ -20,6 +25,8 @@ createApp({
             }).catch(error => {
                 console.log(error);
             })
+
+        console.log(vals);
     },
     downloadStl() {
         const objectUrl = URL.createObjectURL(new Blob([this.model], { type: 'text/stl' }));
@@ -46,24 +53,34 @@ createApp({
 
 
 function sliderInput(props) {
+    vals[props.name] = props.value;
     return {
         $template: '#slider-template',
         name: props.name,
+        label: props.label,
         description: props.description,
         min: props.min,
         max: props.max,
         step: props.step,
         value: props.value,
+        setVal() {
+            vals[props.name] = parseInt(this.value);
+        }
     }
 }
 
 function selectInput(props) {
+    vals[props.name] = props.value;
     return {
         $template: '#select-template',
         name: props.name,
+        label: props.label,
         description: props.description,
         options: props.options,
         value: props.value,
+        setVal() {
+            vals[props.name] = this.value;
+        }
     }
 }
 
